e4b962772c0b68420a03b1677b24a71678c23146
Joe Hoye Dow <luxunisgod123@gmail.com>
2025-03-17 15:39:51 +0800

    Add files via upload

---
+++ Undergraduate_class/chap1/ex0101.cpp
@@ -0,0 +1,12 @@
+//程序用于求一个数的平方
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,square;
+	cout << "num=";
+	cin >> num;
+	square = num * num;
+	cout << "num的平方为：" << square << endl;
+	return 0;
+}
---
+++ Undergraduate_class/chap1/ex0103.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+int main()
+{
+	float r;
+	cout << "ÇëÊäÈë°ë¾¶=" << endl;
+	cin >> r;
+	cout << "Ãæ»ý=" << PI * r * r << endl;
+	
+	cout << 5 / 2 << 5.0 / 2 << endl;
+return 0;
+}
